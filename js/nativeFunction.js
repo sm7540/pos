85bd21eefc4542624749c36eba3950751cb83de2
sangmoon <jeonsangmoon7540@gmail.com>
2022-09-25 20:57:08 +0900

    test

---
+++ js/nativeFunction.js
@@ -0,0 +1,51 @@
+const APP_NAME = "스스시";
+
+//네이티브 얼랏
+async function n_alert(msg){
+    await window.api.alert(APP_NAME,msg);
+}
+//네이티브 에러
+async function n_error(msg){
+    await window.api.error(APP_NAME,msg);
+}
+//네이티브 CONFIRM
+async function n_confirm(msg){
+    const result = await window.api.confirm(APP_NAME,msg);
+    //0 : 예
+    //1 : 아니오
+    return result;
+}
+
+//창사이즈 변경
+async function n_resize(width,height){
+    await window.api.resize(width,height);
+}
+
+//함수로 만들어놨지만 직접 API접근해서 사용해도 상관없다
+async function n_setItem(key,value){
+    await window.api.setItem(key,value);
+}
+
+async function n_getItem(key){
+    var item = await window.api.getItem(key);
+    //console.log("LOGIN ITEM RESULT",item);
+    return item;
+    
+}
+
+async function n_clearItem(){
+    await window.api.clearItem();
+}
+
+async function n_getVersion(){
+    return window.api.getVersion();
+}
+
+
+async function n_encrypt(text){
+    return window.api.encrypt(text);
+}
+
+async function n_decrypt(text){
+    return window.api.decrypt(text);
+}
---
+++ preload.js
@@ -0,0 +1,44 @@
+const { contextBridge, ipcRenderer } = require('electron');
+contextBridge.exposeInMainWorld(
+    'api',
+	{
+		error: async (title,msg) => {
+			await ipcRenderer.invoke('showErrorBox',[title,msg]);
+		},
+		alert : async (title,msg) => {
+            await ipcRenderer.invoke('showMessageBox',[title,msg]);
+		},
+		confirm : async (title,msg)=>{
+			var result = await ipcRenderer.invoke('showConfirmBox',[title,msg]);
+			//console.log("PRELOAD showConfirmBox",result['response']);
+			return result['response'];
+		},
+        showDialog: async () => await ipcRenderer.invoke('dialog:open'),
+		resize : async (width,height) => {
+			await ipcRenderer.invoke('windowSizeChange',[width,height]);
+		},
+		setItem : async (key,value)=>{
+			//console.log("setItem Call",key,value);
+			await ipcRenderer.invoke('setItem',key,value);
+		},
+		getItem : async (key)=>{
+			const item = await ipcRenderer.invoke('getItem',key);
+			//console.log("PRELOAD GET ITEM RESULT",item);
+			return item;
+			
+		},
+		clearItem : async () =>{
+			await ipcRenderer.invoke('clearItem');
+		},
+		getVersion : async() => {
+			const version = await ipcRenderer.invoke('getVersion');
+			return version;
+		},
+		encrypt : async(text) => {
+			return await ipcRenderer.invoke('encrypt',text);
+		},
+		decrypt : async(text) => {
+			return await ipcRenderer.invoke('decrypt',text);
+		},
+    }
+)
